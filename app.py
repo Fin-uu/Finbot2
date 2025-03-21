9a1ff0e427d85b55649d4e9332eef04c13520700
tzuyu <tzuyu@exentric.com.tw>
2025-03-21 17:13:09 +0800

    Your commit message

--- app.py
+++ app.py
@@ -48,12 +48,13 @@ def callback():
 def handle_message(event):
     # 取得使用者傳來的訊息
     user_message = event.message.text
-
+    payer
+    user
     # 這裡可以加入您自己的邏輯來處理使用者訊息
-    if "你好" in user_message:
+    if "哩賀" in user_message:
         reply_text = "你好！很高興認識你！"
-    elif "天氣" in user_message:
-        reply_text = "今天天氣真好！"
+    elif "天氣如何" in user_message:
+        reply_text = "今天天氣很好！"
     else:
         reply_text = f"你說了：{user_message}"
 
